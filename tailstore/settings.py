408dfa966b458a7e586325d520d3d3a8753bdf5a
milad <miladeisanezhad1@gmail.com>
2026-06-25 15:03:07 +0330

    logging for setting

--- tailstore/settings.py
+++ tailstore/settings.py
@@ -95,3 +95,15 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
