aaebbd90781b145cdd2c83bce76ce01091dcfa14
milad <miladeisanezhad1@gmail.com>
2026-06-28 19:44:48 +0330

    Add Supabase S3 storage for media files

--- tailstore/settings.py
+++ tailstore/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
     "store",
 ]
 
@@ -97,8 +98,23 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+if DEBUG:
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
+else:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+    AWS_ACCESS_KEY_ID = os.environ.get("SUPABASE_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("SUPABASE_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("SUPABASE_BUCKET_NAME")
+    AWS_S3_ENDPOINT_URL = os.environ.get("SUPABASE_ENDPOINT_URL")
+    AWS_S3_REGION_NAME = os.environ.get("SUPABASE_REGION")
+
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+    MEDIA_URL = os.environ.get("SUPABASE_PROJECT_URL") + "/storage/v1/object/public/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
